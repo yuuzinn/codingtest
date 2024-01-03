11c09adf8d153c380a84aabc9d0cf892e08f2b67
yuuzin <s110528@nate.com>
2024-01-03 17:50:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 54.67 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch); 
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
