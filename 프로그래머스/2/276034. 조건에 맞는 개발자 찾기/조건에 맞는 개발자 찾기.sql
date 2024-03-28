7501b79f83f4342b536720b7e32b0d1a1980fd22
yuuzin <s110528@nate.com>
2024-03-28 16:47:16 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,17 @@
+-- 코드를 작성해주세요
+-- & 연산으로 비트값 가져오기
+SELECT
+    ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM
+    DEVELOPERS
+WHERE
+    (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python'))
+    OR (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#'))
+ORDER BY
+    ID;
+  
+  
+  
