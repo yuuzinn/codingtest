deb44d9815c0cb2f688f1e432741abf2dbf7eb74
dntkdgns <s110528@nate.com>
2025-03-22 17:52:21 +0900

    Time: 2 ms (97.68%), Space: 43.2 MB (74.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    // 문자열 길이가 다르면 바로 false 반환
-    if (s.length() != t.length()) {
-        return false;
-    }
-
-    // 문자 빈도 저장
-    Map<Character, Integer> sMap = new HashMap<>();
-    Map<Character, Integer> tMap = new HashMap<>();
-
-    // 각 문자열의 문자를 순회하면서 빈도수 저장
-    for (int i = 0; i < s.length(); i++) {
-        char cs = s.charAt(i);
-        char ct = t.charAt(i);
-        sMap.put(cs, sMap.getOrDefault(cs, 0) + 1);
-        tMap.put(ct, tMap.getOrDefault(ct, 0) + 1);
-    }
-
-    // sMap의 각 키에 대해 tMap에 같은 키가 존재하며 값이 동일한지 확인
-    for (Character key : sMap.keySet()) {
-        if (!tMap.containsKey(key) || !sMap.get(key).equals(tMap.get(key))) {
-            return false;
+        int[] count = new int[26];
+        
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
         }
+        
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
-    return true;
-}
-
 }
