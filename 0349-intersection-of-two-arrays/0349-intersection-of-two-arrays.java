fd53ccf80fd1a3173ac3597d04f0c498dee2d930
dntkdgns <s110528@nate.com>
2025-03-23 19:15:30 +0900

    Time: 2 ms (96.39%), Space: 43.4 MB (62.8%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,37 +1,24 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        // nums2 배열을 Set을 통해 중복제거함
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        // nums1의 모든 원소를 저장하는 HashSet (중복 제거)
         Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        for (int j : nums1) {
-            set1.add(j);
+        for (int num : nums1) {
+            set1.add(num);
         }
-        for (int j : nums2) {
-            set2.add(j);
-        }
-        // set 의 원소를 하나씩 nums1에 돌면서 찾아보기, count 만큼 배열 자리 만들기
-        if (set1.size() < set2.size()) {
-            Iterator<Integer> iterator = set2.iterator();
-            while (iterator.hasNext()) {
-                int num = iterator.next();
-                if (set1.contains(num)) {
-                    list.add(num);
-                }
-            }
-        } else {
-            Iterator<Integer> iterator = set1.iterator();
-            while (iterator.hasNext()) {
-                int num = iterator.next();
-                if (set2.contains(num)) {
-                    list.add(num);
-                }
+        
+        // nums2를 순회하며, set1에 있는 값만 intersection에 추가 (중복 없이)
+        Set<Integer> intersection = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersection.add(num);
             }
         }
-
-        int[] result = new int[list.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = list.get(i);
+        
+        // HashSet에 저장된 교집합 원소들을 int 배열로 변환
+        int[] result = new int[intersection.size()];
+        int i = 0;
+        for (int num : intersection) {
+            result[i++] = num;
         }
         return result;
     }
