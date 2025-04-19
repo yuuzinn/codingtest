8955fd24ac17d0e24446f9cb6f342795dace2d54
dntkdgns <s110528@nate.com>
2025-04-19 20:23:06 +0900

    Time: 2 ms (97.44%), Space: 42 MB (32.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char open = stack.pop();
+                if (c == ')' && open != '(') return false;
+                if (c == ']' && open != '[') return false;
+                if (c == '}' && open != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
